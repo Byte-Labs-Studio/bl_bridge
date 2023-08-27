90c47679c74b0de58468749559263db5bc13e374
Frowmza <hamzaaitali55@gmail.com>
2023-08-27 19:44:57 +0100

    this is huge than i expected

--- server/modules/core/esx.lua
+++ server/modules/core/esx.lua
@@ -3,4 +3,41 @@ if GetResourceState('es_extended') ~= 'started' then
     return
 end
 
-return exports["es_extended"]:getSharedObject()
+local Core = {}
+local shared = exports["es_extended"]:getSharedObject()
+local Utils = require 'utils'
+local functionsOverride = {
+    getBalance = {
+        originalMethod = 'getAccount',
+        modifier = function(originalFun, type)
+            return originalFun(type == 'cash' and 'money' or type).money
+        end
+    },
+    removeBalance = {
+        originalMethod = 'removeAccountMoney',
+        modifier = function(originalFun, type, amount)
+            return originalFun(type == 'cash' and 'money' or type, amount)
+        end
+    },
+    addBalance = {
+        originalMethod = 'addAccountMoney',
+        modifier = function(originalFun, type, amount)
+            return originalFun(type == 'cash' and 'money' or type, amount)
+        end
+    }
+}
+
+
+function Core.CommandAdd(name, permission, cb, suggestion, flags)
+    shared.RegisterCommand(name, permission, cb, flags.allowConsole, suggestion)
+end
+
+function Core.GetPlayer(src)
+    local player = shared.GetPlayerFromId(src)
+    if not player then return end
+    local wrappedPlayer = Utils.retreiveData(player, functionsOverride)
+    return wrappedPlayer
+end
+
+Core.Players = shared.Players
+return Core
--- server/modules/core/qb.lua
+++ server/modules/core/qb.lua
@@ -1,3 +1,40 @@
--- this also have inventory exports??
+if GetResourceState('qb-core') ~= 'started' then
+    error('The imported file from the chosen framework isn\'t starting')
+    return
+end
 
-return exports['qb-core']:GetCoreObject()
+local Core = {}
+local shared = exports['qb-core']:GetCoreObject()
+local Utils = require 'utils'
+local functionsOverride = {
+    getBalance = {
+        originalMethod = 'GetMoney',
+    },
+    removeBalance = {
+        originalMethod = 'RemoveMoney',
+    },
+    addBalance = {
+        originalMethod = 'AddMoney',
+    }
+}
+
+function Core.CommandAdd(name, permission, cb, suggestion, flags)
+    if type(name) == 'table' then
+        for _,command in ipairs(name) do
+            shared.Commands.Add(command, suggestion.help, suggestion.arguments, flags.argsrequired, cb, permission)
+        end
+        return
+    end
+    shared.Commands.Add(name, suggestion.help, suggestion.arguments, flags.argsrequired, cb, permission)
+end
+
+
+function Core.GetPlayer(src)
+    local player = shared.Functions.GetPlayer(src)
+    if not player then return end
+    local wrappedPlayer = Utils.retreiveData(player.Functions, functionsOverride)
+    return wrappedPlayer
+end
+
+Core.Players = shared.Players
+return Core
