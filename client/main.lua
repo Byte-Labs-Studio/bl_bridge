f9f8196335b1ed1c002d14bb8ddabaa755480389
Frowmza <66181451+Frowmza@users.noreply.github.com>
2023-11-16 19:08:55 +0100

    last

--- client/main.lua
+++ client/main.lua
@@ -5,15 +5,12 @@ local moduleNames = {
     "context",
 }
 
-print('choosed framework: ' .. json.encode(convars))
-
 for _, moduleName in ipairs(moduleNames) do
     local framework = convars[moduleName]
     if frameworks[framework] then
         local fomartedModule = ("%s.%s.%s"):format(clientDir, moduleName, framework)
         local success, module = pcall(require, fomartedModule)
         if success then
-            print('module loaded: ' .. moduleName)
             Framework[moduleName] = module
         else
             error(("Error loading module %s: %s"):format(moduleName, module))
--- server/main.lua
+++ server/main.lua
@@ -12,15 +12,12 @@ lib.callback.register('UUID', function(_, num)
     return UUID(num)
 end)
 
-print('choosed framework: ' .. json.encode(convars))
-
 for _, moduleName in ipairs(moduleNames) do
     local framework = convars[moduleName]
     if frameworks[framework] then
         local fomartedModule = ("%s.%s.%s"):format(serverDir, moduleName, framework)
         local success, module = pcall(require, fomartedModule)
         if success then
-            print('module loaded: ' .. moduleName)
             Framework[moduleName] = module
         else
             error(("Error loading module %s: %s"):format(moduleName, module))
