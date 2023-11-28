f6975adc5193cbfc7c11a0e130e648867f583006
Xirvin <xander228irvin@gmail.com>
2023-11-28 13:33:38 +1100

    types for context

--- _types.lua
+++ _types.lua
@@ -1,5 +1,5 @@
 
-
+---@alias IconProp 'fas' | 'far' | 'fal' | 'fat' | 'fad' | 'fab' | 'fak' | 'fass'
 ---@alias NotificationPosition 'top' | 'top-right' | 'top-left' | 'bottom' | 'bottom-right' | 'bottom-left' | 'center-right' | 'center-left'
 
 ---@class NotificationParams
@@ -9,3 +9,31 @@
 ---@field position? NotificationPosition
 ---@field status? 'info' | 'warning' | 'success' | 'error'
 ---@field id? number
+
+---@class ContextMenuItem
+---@field title? string
+---@field menu? string
+---@field icon? string | {[1]: IconProp, [2]: string};
+---@field iconColor? string
+---@field image? string
+---@field progress? number
+---@field onSelect? fun(args: any)
+---@field arrow? boolean
+---@field description? string
+---@field metadata? string | { [string]: any } | string[]
+---@field disabled? boolean
+---@field event? string
+---@field serverEvent? string
+---@field args? any
+
+---@class ContextMenuArrayItem : ContextMenuItem
+---@field title string
+
+---@class ContextMenuProps
+---@field id string
+---@field title string
+---@field menu? string
+---@field onExit? fun()
+---@field onBack? fun()
+---@field canClose? boolean
+---@field options { [string]: ContextMenuItem } | ContextMenuArrayItem[]
