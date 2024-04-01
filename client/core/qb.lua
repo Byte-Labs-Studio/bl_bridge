0ac0c2f5cc4159d6d744d6d834c74b8733799ffc
Frowmza <66181451+Frowmza@users.noreply.github.com>
2024-04-01 15:23:08 +0000

    fix(client/inventory): fix inventory not refreshing

--- client/core/qb.lua
+++ client/core/qb.lua
@@ -42,7 +42,7 @@ local coreFunctionsOverride = {
                     return {
                         cid = data.citizenid,
                         money = data.money,
-                        inventory = type(data.inventory) == 'string' and json.decode(data.inventory) or data.inventory,
+                        inventory = type(data.items) == 'string' and json.decode(data.items) or data.items,
                         job = { name = job.name, label = job.label, onDuty = job.onduty, isBoss = job.isboss, type = job.type, grade = { name = job.grade.level, label = job.grade.name, salary = job.payment } },
                         gang = { name = gang.name, label = gang.label, isBoss = gang.isboss, grade = { name = gang.grade.level, label = gang.grade.label } },
                         firstName = charinfo.firstname,
