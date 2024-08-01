036945c0da62274b25184f87d8dd17622ae542a0
DevX32 <dishantacnc389@gmail.com>
2024-08-01 20:32:00 +0545

    typo fix

--- client/core/esx.lua
+++ client/core/esx.lua
@@ -25,7 +25,7 @@ function Core.getPlayerData()
     local job = player.job
     local gang = player.gang
     local charinfo = player.charinfo
-    local year, month, day = charinfo.birthdate:match('(%d+)-(%d+)-($d+)')
+    local year, month, day = charinfo.birthdate:match('(%d+)-(%d+)-(%d+)')
 
     local formattedJob = {
         name = job.name,
@@ -43,7 +43,7 @@ function Core.getPlayerData()
     local formattedGang = {
         name = gang.name,
         label = gang.label,
-        isBoss = gang.isBoss,
+        isBoss = gang.isboss,
         grade = {
             name = gang.grade.level,
             label = gang.grade.label
@@ -60,7 +60,7 @@ function Core.getPlayerData()
         lastName = charinfo.lastName or 'Unknown',
         phone = charinfo.phone or '0000000',
         gender = charinfo.gender == 1 and 'female' or 'male',
-        dob = ('%s/%s/%S'):format(month, day, year) -- DD/MM/YYYY
+        dob = ('%02d/%02d/%04d'):format(month, day, year) -- DD/MM/YYYY
     }
 end
 
