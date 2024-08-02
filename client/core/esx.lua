8fef21ee8adeccb13a84c51d46860db8640be34d
DevX32 <dishantacnc389@gmail.com>
2024-08-02 06:24:42 +0545

    thats why i shouldnt work while feeling sleepy

--- client/core/esx.lua
+++ client/core/esx.lua
@@ -21,45 +21,31 @@ end)
 local shared = exports[coreName]:getSharedObject()
 
 function Core.getPlayerData()
-    local player = shared.GetPlayerData()
-    local job = player.job
-    local gang = player.gang
-    local charinfo = player.charinfo
-    local year, month, day = charinfo.birthdate:match('(%d+)-(%d+)-(%d+)')
+    local data = shared.GetPlayerData()
+    local job = data.job
+    local month, day, year = data.dateofbirth and data.dateofbirth:match("(%d+)/(%d+)/(%d+)") or 00, 00, 24
 
     local formattedJob = {
         name = job.name,
         label = job.label,
-        onDuty = job.onduty,
-        isBoss = job.isboss,
-        type = job.type,
+        onDuty = true,
+        isBoss = false,
         grade = {
-            name = job.grade.level,
-            label = job.grade.name,
-            salary = job.payment
-        }
-    }
-
-    local formattedGang = {
-        name = gang.name,
-        label = gang.label,
-        isBoss = gang.isboss,
-        grade = {
-            name = gang.grade.level,
-            label = gang.grade.label
+            name = job.grade,
+            label = job.grade_label,
+            salary = job.grade_salary
         }
     }
 
     return {
-        cid = player.citizenid,
-        money = player.money or 0,
-        inventory = type(player.items) == 'string' and json.decode(player.items) or player.items,
+        cid = data.identifier,
+        money = data.money or 0,
+        inventory = data.inventory or {},
         job = formattedJob,
-        gang = formattedGang,
-        firstName = charinfo.firstName or 'Unknown',
-        lastName = charinfo.lastName or 'Unknown',
-        phone = charinfo.phone or '0000000',
-        gender = charinfo.gender == 1 and 'female' or 'male',
+        firstName = data.firstName or 'Unknown',
+        lastName = data.lastName or 'Unknown',
+        phone = data.phone_number or '0',
+        gender = data.sex == 'm' and 'male' or 'female',
         dob = ('%02d/%02d/%04d'):format(month, day, year) -- DD/MM/YYYY
     }
 end
--- client/core/nd.lua
+++ client/core/nd.lua
@@ -32,46 +32,32 @@ end)
 local shared = exports[coreName]
 
 function Core.getPlayerData()
-    local player = shared.GetPlayerData()
-    local job = player.job
-    local gang = player.gang
-    local charinfo = player.charinfo
-    local year, month, day = charinfo.birthdate:match('(%d+)-(%d+)-(%d+)')
+    local data = shared.getPlayer()
+    local jobData = data.jobInfo
+    local year, month, day = data.dob:match("(%d+)-(%d+)-(%d+)")
 
     local formattedJob = {
-        name = job.name,
-        label = job.label,
-        onDuty = job.onduty,
-        isBoss = job.isboss,
-        type = job.type,
+        name = data.job,
+        label = jobData.label,
+        onDuty = true,
+        isBoss = jobData.isBoss,
         grade = {
-            name = job.grade.level,
-            label = job.grade.name,
-            salary = job.payment
-        }
-    }
-
-    local formattedGang = {
-        name = gang.name,
-        label = gang.label,
-        isBoss = gang.isboss,
-        grade = {
-            name = gang.grade.level,
-            label = gang.grade.label
+            name = jobData.rank,
+            label = jobData.rankName,
+            salary = 0
         }
     }
 
     return {
-        cid = player.citizenid,
-        money = player.money or 0,
-        inventory = type(player.items) == 'string' and json.decode(player.items) or player.items,
+        cid = data.id,
+        money = data.cash,
+        inventory = type(data.inventory) == 'string' and json.decode(data.inventory) or data.inventory,
         job = formattedJob,
-        gang = formattedGang,
-        firstName = charinfo.firstName or 'Unknown',
-        lastName = charinfo.lastName or 'Unknown',
-        phone = charinfo.phone or '0000000',
-        gender = charinfo.gender == 1 and 'female' or 'male',
-        dob = ('%02d/%02d/%04d'):format(month, day, year) -- DD/MM/YYYY
+        firstName = data.firstname,
+        lastName = data.lastname,
+        phone = 'Unknown',
+        gender = string.lower(data.gender),
+        dob = ('%s/%s/%s'):format(month, day, year),
     }
 end
 
