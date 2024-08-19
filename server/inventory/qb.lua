aae2e1ee87e762748e0f4e0493f1a19a86860118
Frowmza <66181451+Frowmza@users.noreply.github.com>
2024-08-19 15:29:30 +0100

    fix(server/qb): retrieve methods directly from inventory exports instead of core player data

--- server/inventory/qb.lua
+++ server/inventory/qb.lua
@@ -1,55 +1,69 @@
+local utils = require 'utils'
+local retreiveExportsData = utils.retreiveExportsData
 local overrideFunction = {}
 local registeredInventories = {}
-overrideFunction.methods = {
-    Functions = {
-        addItem = {
-            originalMethod = 'AddItem',
-            modifier = {
-                passSource = true,
-                effect = function(originalFun, src, name, amount, metadata, slot)
-                    TriggerClientEvent('inventory:client:ItemBox', src, name, "add")
-                    return originalFun(name, amount, slot, metadata)
-                end
-            }
-        },
-        removeItem = {
-            originalMethod = 'RemoveItem',
-            modifier = {
-                passSource = true,
-                effect = function(originalFun, src, name, amount, slot)
-                    TriggerClientEvent('inventory:client:ItemBox', src, name, "remove")
-                    return originalFun(name, amount, slot)
-                end
-            }
-        },
-        getItem = {
-            originalMethod = 'GetItemByName',
-            modifier = {
-                effect = function(originalFun, itemName)
-                    local data = originalFun(itemName)
-                    if not data then
-                        return false, 'Item not exist or you don\'t have it'
-                    end
-                    return {
-                        label = data.label,
-                        name = data.name,
-                        weight = data.weight,
-                        slot = data.slot,
-                        close = data.shouldClose,
-                        stack = not data.unique,
-                        metadata = data.info ~= '' and data.info or {},
-                        count = data.amount or 1
-                    }
-                end
-            }
+
+
+overrideFunction.methods = retreiveExportsData(exports['qb-inventory'], {
+    addItem = {
+        originalMethod = 'AddItem',
+        modifier = {
+            passSource = true,
+            effect = function(originalFun, src, name, amount, metadata, slot)
+                TriggerClientEvent('inventory:client:ItemBox', src, name, "add", amount)
+                TriggerClientEvent('qb-inventory:client:ItemBox', src, name, "add", amount)
+                return originalFun(src, name, amount, slot, metadata)
+            end
         }
     },
-    PlayerData = {
-        items = {
-            originalMethod = 'items',
-        },
-    }
-}
+    removeItem = {
+        originalMethod = 'RemoveItem',
+        modifier = {
+            passSource = true,
+            effect = function(originalFun, src, name, amount, slot)
+                TriggerClientEvent('inventory:client:ItemBox', src, name, "remove", amount)
+                TriggerClientEvent('qb-inventory:client:ItemBox', src, name, "remove", amount)
+                return originalFun(src, name, amount, slot)
+            end
+        }
+    },
+    setMetaData = {
+        originalMethod = 'SetItemData',
+        modifier = {
+            passSource = true,
+            effect = function(originalFun, src, slot, data)
+                local item = exports['qb-inventory']:GetItemBySlot(src, slot)
+
+                if not item then return end
+                if type(data) ~= 'table' then return end
+
+                originalFun(src, item.name, 'info', data)
+            end
+        }
+    },
+    getItem = {
+        originalMethod = 'GetItemByName',
+        modifier = {
+            passSource = true,
+            effect = function(originalFun, src, itemName)
+                local data = originalFun(src, itemName)
+                if not data then
+                    return false, 'Item not exist or you don\'t have it'
+                end
+                return {
+                    label = data.label,
+                    name = data.name,
+                    weight = data.weight,
+                    slot = data.slot,
+                    close = data.shouldClose,
+                    stack = not data.unique,
+                    metadata = data.info ~= '' and data.info or {},
+                    count = data.amount or 1
+                }
+            end
+        }
+    },
+})
 
 function overrideFunction.registerInventory(id, data)
     local type, name, items, slots, maxWeight in data
@@ -67,7 +81,7 @@ function overrideFunction.registerInventory(id, data)
     }
 end
 
-require'utils'.register('bl_bridge:validInventory', function(_, invType, invId)
+utils.register('bl_bridge:validInventory', function(_, invType, invId)
     local inventory = registeredInventories[('%s-%s'):format(invType, invId)]
     if not inventory then return end
     return inventory
