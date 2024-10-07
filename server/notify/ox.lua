835dcaae9742da78f26f913df07f0d76d32b01bb
Frowmza <66181451+Frowmza@users.noreply.github.com>
2024-10-07 12:36:37 +0100

    fix(ox/server/notify): use event instead

--- server/notify/ox.lua
+++ server/notify/ox.lua
@@ -2,8 +2,8 @@
 
 ---@param source number Source of player
 ---@param data NotificationParams Notification data
-function notify(source, data)
-    exports.ox_lib:notify(source, data)
+local function notify(source, data)
+    TriggerClientEvent('ox_lib:notify', source, data)
 end
 
 return notify
